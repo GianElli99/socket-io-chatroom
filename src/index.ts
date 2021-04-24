ddf274068f4503d3cff932027881ea1adbe91bf2
GianElli99 <gianelli99@hotmail.com>
2021-04-24 19:04:56 -0300

    chat app first version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/frontend/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
